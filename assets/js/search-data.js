c99df66051135895903e297e238add97088a9532
weslleyspereira <weslley.spereira@gmail.com>
2026-01-03 18:54:13 +0000

    Deploying to gh-pages from @ weslleyspereira/weslleyspereira.github.io@81b920eaefcebd5e5d7db2449992ba4d632e9d90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -437,6 +437,13 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-cv',
+        title: 'CV',
+        section: 'Socials',
+        handler: () => {
+          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
